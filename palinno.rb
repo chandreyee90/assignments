b534188b75d4479a4b9cc3264067bac304bb75dd
chandreyee90 <sen.chandreyee2011@gmail.com>
2015-03-12 13:47:35 +0530

    2nd days assignments

---
+++ palinno.rb
@@ -0,0 +1,16 @@
+class Palin
+def get_value()
+
+puts "Enter the number:"
+
+
+string = gets.chomp
+for i in 111..666
+ string.reverse == string
+ puts "#{string} is palindrome"
+end
+end
+end
+
+s=Palin.new
+s.get_value()
